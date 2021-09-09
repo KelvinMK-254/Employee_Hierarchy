3acaf460e318253859640cd16f396efad17aed6a
KelvinMK-254 <62746481+KelvinMK-254@users.noreply.github.com>
2021-09-09 12:23:43 +0300

    Add files via upload

---
+++ EmployeeHierarchy/EmployeeHierarchy/Exceptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeHierarchy
+{
+    class Exceptions : Exception
+    {
+        public Exceptions(string message) : base(message)
+        {
+        }
+    }
+
+
+    class SalaryInvalid : Exception
+    {
+        public SalaryInvalid(string message) : base(message)
+        {
+        }
+    }
+}
